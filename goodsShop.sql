f95fe44124a6f3c0783c015a47954807ce4949eb
guozhitao-theo <2943439858@qq.com>
2019-09-24 18:58:40 +0800

    11

---
+++ goodsShop.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.40 : Database - school
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`school` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `school`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(200) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` varchar(2) DEFAULT NULL,
+  `class` varchar(10) DEFAULT NULL,
+  `form` varchar(50) DEFAULT NULL,
+  `english` double(3,1) DEFAULT NULL,
+  `math` double(3,1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`age`,`gender`,`class`,`form`,`english`,`math`) values (1,'小明',18,'男','1','重庆',50.2,80.5),(2,'小张',20,'女','2','四川',78.5,78.5),(3,'小李',40,'男','2','吉林',64.5,78.5),(4,'小黄',60,'女','3','西疆',99.9,75.3),(5,'王小明',20,'女','1','山东',90.6,45.2),(6,'张小明',10,'男','4','江西',12.3,45.5),(7,'黄小明',1,'女','1','重庆',75.5,57.3),(8,'甄晓丽',2,'女','4','吉林',78.0,75.0);
+
+/*Table structure for table `students` */
+
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE `students` (
+  `ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `students` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
