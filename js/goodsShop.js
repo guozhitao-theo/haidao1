19d91cc3c2f59c3bd09560e0476ace6855f995c7
guozhitao-theo <2943439858@qq.com>
2019-09-24 22:13:26 +0800

    1

---
+++ js/goodsShop.js
@@ -0,0 +1,15 @@
+// ajax请求
+
+$.ajax({
+    url:"http://127.0.0.1:3000/index",
+    type:"get",
+    data:{
+
+    },
+    datatype:"json",
+    success:function(res){
+        
+
+        }
+    }
+})
