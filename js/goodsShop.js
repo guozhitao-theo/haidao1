82f116f8bb13ea9b4d4ee7627b4545813a77fe76
guozhitao-theo <2943439858@qq.com>
2019-09-25 17:37:48 +0800

    w

--- js/goodsShop.js
+++ js/goodsShop.js
@@ -1,5 +1,8 @@
 // ajax请求
 
+let goodsBox = $(".shops-goods-box")
+console.log(goodsBox)
+var shopHtml = ''
 $.ajax({
     url:"http://127.0.0.1:3000/index",
     type:"get",
@@ -8,8 +11,36 @@ $.ajax({
     },
     datatype:"json",
     success:function(res){
+        // res是一个数组，循环数组得到里面的值
+        // for(let item of res.data){
+         
+        //     // 通过模板字符串将数据渲染
+            
+        //     // 定义一个模板字符串
+        //     shopHtml += `
+        // <li class="shops-goods-content">
+        //     <div>
+        //         <input type="checkbox">
+        //     </div>
+        //     <div class="goods-num">${item.id}</div>
+        //     <div class="goods-name">${item.name}</div>
+        //     <div class="goods-classify text-hidden">${item.text}</div>
+        //     <div class="goods-price">￥${item.price}</div>
+        //     <div class="goods-stock">${item.stock}</div>
+        //     <div class="goods-order">1</div>
+        //     <div class="goods-up">对</div>
+        //     <div class="goods-option"><a>修改</a> <a>删除</a></div>
+        // </li>`
+
+        // goodsBox.html(shopHtml)
+        // }
         
 
+        // 模板引擎template(需要渲染的ID ，要渲染的数据 )
+
+        var HTML = template('tablist',res)
+        console.log(res)
+        console.log(HTML)
+        goodsBox.html(HTML)
         }
-    }
 })
--- js/server.js
+++ js/server.js
@@ -4,7 +4,7 @@ let mysql = require('mysql')
 
 // 创建一个数据库链接
 let dbConnect = mysql.createConnection({
-    host:"192.168.97.251~",
+    host:"127.0.0.1",
     user: "root",
     password: "root",
     port:3306,
@@ -15,6 +15,12 @@ dbConnect.connect()
 
 // 执行express方法
 let app = express()
+app.all("*",function(req,res,nextfunction){
+    res.header("Access-Control-Allow-Origin","*")
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    nextfunction()
+})
+
 
 // 监听get地址
 app.get("/index",function(req,res){  
@@ -23,7 +29,7 @@ app.get("/index",function(req,res){
     dbConnect.query("select * from shop",function(err,data){
         // data为一个数组,内容为对象
         for(let item of data){
-            console.log(item)
+            console.log(data)
         }
         res.json({data})  
         
